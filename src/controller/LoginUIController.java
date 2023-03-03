287638321bceb462a048ad2ff20f49531f21c111
ZeroSTF <kateskatous@gmail.com>
2023-03-03 05:03:37 +0100

    merge

--- src/controller/LoginUIController.java
+++ src/controller/LoginUIController.java
@@ -15,7 +15,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
